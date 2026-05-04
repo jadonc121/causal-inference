52a8ae5874e6f5a8d5a0dc158174814cb9831930
Jadon Costa <jadon.costa@ucdenver.edu>
2026-05-03 20:31:08 -0600

    Added custom dataframe display to spark

--- src/utils/spark_utils/spark_session.py
+++ src/utils/spark_utils/spark_session.py
@@ -1,4 +1,32 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
+
+def _patch_dataframe_display(row_limit: int = 100):
+    """
+    Monkeypatches a display method onto the Spark DataFrame object to mirror the display behavior in Databricks.
+
+    Parameters:
+        - row_limit: int, default = 100
+            This is the max row length the returned dataframe will show.
+
+    Returns:
+        - Pandas DataFrame display
+
+    Example: 
+        df = spark.createDataFrame(...)
+        df.display()
+    """
+
+    try:
+        from IPython.display import display as ipython_display
+
+        def custom_display(self, limit: int = row_limit):
+            ipython_display(self.limit(limit).toPandas())
+
+        DataFrame.display = custom_display
+
+    except ImportError:
+        # Ignores if not in a Jupyter notebook.
+        pass
 
 def get_spark_session(app_name: str = "causal_inference_project") -> SparkSession:
     """
@@ -11,12 +39,16 @@ def get_spark_session(app_name: str = "causal_inference_project") -> SparkSessio
 
     Note: Log level of ERROR will ignore numerical errors! Convert back to WARN to view these.
     """
+    _patch_dataframe_display()
+
     spark = (
         SparkSession.builder
         .master("local[*]")  # The [*] tells Spark to use all available CPU cores on your Mac
         .appName(app_name)
         .config("spark.driver.memory", "4g")  # Allocates 4GB of RAM to the local driver
         .config("spark.sql.shuffle.partitions", "4")  # Optimizes shuffles for a local machine
+        # .config("spark.sql.repl.eagerEval.enabled", True)
+        # .config("spark.sql.repl.eagerEval.maxNumRows", 100)
         .getOrCreate()
     )
     
